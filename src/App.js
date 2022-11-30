7e6be0c582ecbe381fd872de51c8f33220d35b6c
Dickson-Pinheiro <dicksonpinheiro4@gmail.com>
2022-11-30 00:57:04 -0300

    início do layout com css disponibilizado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./img/logo.png"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="screen-container">
+      <div className="logo-container">
+        <img src={logo} />
+        <h1>ZapRecall</h1>
+      </div>
+      <div className="pergunta-fechada">
+        <p>pergunta 1</p>
+        <ion-icon name="play-outline"></ion-icon>
+      </div>
+      <div className="pergunta-fechada">
+        <p>pergunta 2</p>
+        <ion-icon name="play-outline"></ion-icon>
+      </div>
+      <div className="pergunta-fechada">
+        <p>pergunta 3</p>
+        <ion-icon name="play-outline"></ion-icon>
+      </div>
+      <div className="pergunta-fechada">
+        <p>pergunta 4</p>
+        <ion-icon name="play-outline"></ion-icon>
+      </div>
+      <div className="footer-concluidos">
+        0/4 CONCLUÍDOS
+      </div>
     </div>
   );
 }
