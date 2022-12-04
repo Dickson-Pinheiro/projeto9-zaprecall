f684dda6724729a889dd74ff839719aa9fa2cb02
Dickson-Pinheiro <dicksonpinheiro4@gmail.com>
2022-12-03 22:51:00 -0300

    componentes e styled-components adicionados ao projeto

--- src/App.js
+++ src/App.js
@@ -1,32 +1,9 @@
-import logo from "./img/logo.png"
+import MainRecall from "./components/MainRecall";
+
 
 function App() {
   return (
-    <div className="screen-container">
-      <div className="logo-container">
-        <img src={logo} />
-        <h1>ZapRecall</h1>
-      </div>
-      <div className="pergunta-fechada">
-        <p>pergunta 1</p>
-        <ion-icon name="play-outline"></ion-icon>
-      </div>
-      <div className="pergunta-fechada">
-        <p>pergunta 2</p>
-        <ion-icon name="play-outline"></ion-icon>
-      </div>
-      <div className="pergunta-fechada">
-        <p>pergunta 3</p>
-        <ion-icon name="play-outline"></ion-icon>
-      </div>
-      <div className="pergunta-fechada">
-        <p>pergunta 4</p>
-        <ion-icon name="play-outline"></ion-icon>
-      </div>
-      <div className="footer-concluidos">
-        0/4 CONCLUÍDOS
-      </div>
-    </div>
+    <MainRecall />
   );
 }
 
---
+++ src/components/MainRecall.js
@@ -0,0 +1,38 @@
+import styled from "styled-components"
+import FooterRecall from "./FooterRecall";
+import HeaderRecall from "./HeaderRecall";
+import Question from "./Question";
+
+const questionsAndAnswers = [
+    {question: "O que é JSX? ", answer: "Uma extensão de linguagem do JavaScript"}, 
+    {question: "O React é __ ", answer: "uma biblioteca JavaScript para construção de interfaces"},
+    {question: "Componentes devem iniciar com __ ", answer: "letra maiúscula"},
+    {question: "Podemos colocar __ dentro do JSX ", answer: "expressões"},
+    {question: "O ReactDOM nos ajuda __ ", answer: "interagindo com a DOM para colocar componentes React na mesma "},
+    {question: "Usamos npm para __ ", answer: "gerenciar os pacotes necessários e suas dependências"},
+    {question: "Usamos props para __ ", answer: "passar diferentes informações para componentes"},
+    {question: "Usamos estado (state) para __ ", answer: "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente"}
+]
+
+export default function MainRecall() {
+    return (
+        <ContainerMain>
+            <HeaderRecall />
+            {questionsAndAnswers.map((q, i) => <Question question={q.question} answers={q.answer} key={i} position={i+1}/>)}
+            <FooterRecall />
+        </ContainerMain>
+    )
+}
+
+const ContainerMain = styled.div`
+        background-color: #FB6B6B;
+        width: 100%;
+        min-height: 100vh;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin: 0px;
+        padding: 0px;
+        padding-bottom: 200px;
+`
+
